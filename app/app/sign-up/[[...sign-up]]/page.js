c1853f8cc86e696cb65ba57334982fa219e8eecc
Benard Aromi <112564277+aromibenard@users.noreply.github.com>
2024-12-06 12:34:37 +0000

    Clerk auth

---
+++ app/app/sign-up/[[...sign-up]]/page.js
@@ -0,0 +1,9 @@
+import { SignUp } from '@clerk/nextjs'
+
+export default function Page() {
+    return (
+        <div className='grid items-center justify-center w-dvw h-dvh'>
+            <SignUp/>
+        </div>
+    )
+}
--- app/next.config.mjs
+++ app/next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    experimental : {
+        serverActions: {
+            allowedOrigins: ['https://ominous-space-rotary-phone-jvgx6wq4r4q2p9j7-3000.app.github.dev/']
+        }
+    }
+};
 
 export default nextConfig;
