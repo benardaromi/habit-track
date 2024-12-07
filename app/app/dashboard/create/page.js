aec6e64e7fa6c81cc03990cd969478db52f055f3
Benard Aromi <112564277+aromibenard@users.noreply.github.com>
2024-12-07 19:29:56 +0000

    create-habit form

---
+++ app/app/dashboard/create/page.js
@@ -0,0 +1,9 @@
+import CreateHabitForm from "@/components/create-habit-form";
+
+export default function CreatePage() {
+    return (
+        <div className="py-2 flex flex-col">
+            <CreateHabitForm />
+        </div>
+    )
+}
---
+++ app/lib/actions.js
@@ -0,0 +1,11 @@
+'use server'
+
+import { z } from "zod"
+
+const schema = z.object({
+    
+})
+
+export async function recordHabit(formData) {
+
+}
